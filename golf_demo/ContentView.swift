6c32c9e586c1fe9881eb61ad2665e8744f77d14a
adarsh <adarsh0ranjan@gmail.com>
2022-09-15 10:14:42 +0530

    Initial Commit

---
+++ golf_demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  golf_demo
+//
+//  Created by Roro Solutions on 15/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ golf_demo/golf_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  golf_demoApp.swift
+//  golf_demo
+//
+//  Created by Roro Solutions on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct golf_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
