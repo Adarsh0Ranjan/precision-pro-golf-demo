035f0055173c6e0184e3ccdda2d9d4f8c83147fc
adarsh <adarsh0ranjan@gmail.com>
2022-09-19 09:34:28 +0530

    landing screens second commit

---
+++ golf_demo/LandingScreen_5.swift
@@ -0,0 +1,20 @@
+//
+//  LandingScreen_5.swift
+//  golf_demo
+//
+//  Created by Roro Solutions on 18/09/22.
+//
+
+import SwiftUI
+
+struct LandingScreen_5: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandingScreen_5_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingScreen_5()
+    }
+}
